ff7215c2f4e3d6401f33776f95872182e6150ecb
Miguel Molina Jr <molim134@gmail.com>
2020-09-22 13:35:27 -0400

    adding discord bot

---
+++ Chatr/Chatr.Console/DiscordBot.cs
@@ -0,0 +1,20 @@
+﻿namespace Chatr.Console
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+
+    internal class DiscordBot : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
