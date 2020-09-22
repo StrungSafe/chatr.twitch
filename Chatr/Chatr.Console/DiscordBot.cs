fb28cc91f84af47fce156aad4b15665876e122ae
Miguel Molina Jr <molim134@gmail.com>
2020-09-22 14:00:20 -0400

    pasta

--- Chatr/Chatr.Console/DiscordBot.cs
+++ Chatr/Chatr.Console/DiscordBot.cs
@@ -1,20 +1,128 @@
 ﻿namespace Chatr.Console
 {
+    using System;
+    using System.Diagnostics;
     using System.Threading;
     using System.Threading.Tasks;
 
-    using Microsoft.Extensions.Hosting;
+    using Discord;
+    using Discord.Commands;
+    using Discord.WebSocket;
 
-    internal class DiscordBot : IHostedService
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class DiscordBot : IHostedService
     {
-        public Task StartAsync(CancellationToken cancellationToken)
+        private readonly IConfiguration configuration;
+
+        private readonly IHostEnvironment environment;
+
+        private readonly ILogger logger;
+
+        private DiscordSocketClient client;
+
+        public DiscordBot(IConfiguration configuration, ILogger<DiscordBot> logger,
+                   IHostEnvironment environment)
         {
-            return Task.CompletedTask;
+            this.configuration = configuration;
+            this.logger = logger;
+            this.environment = environment;
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            return Task.CompletedTask;
+            try
+            {
+                LogStartup();
+                await commandService.AddModulesAsync();
+                client = new DiscordSocketClient();
+                string token = GetToken();
+                await client.LoginAsync(TokenType.Bot, token);
+                await client.StartAsync();
+
+                client.MessageReceived += HandleMessageReceived;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "There was an unhandled exception during startup.");
+                await StopAsync(cancellationToken);
+            }
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await client.StopAsync();
+            client.Dispose();
+            client = null;
+        }
+
+        private string GetToken()
+        {
+            return configuration.GetSection("AppSettings")["Token"];
+        }
+
+        private async Task HandleMessageReceived(SocketMessage rawMessage)
+        {
+            // Ignore system messages and messages from bots
+            if (!(rawMessage is SocketUserMessage message))
+            {
+                return;
+            }
+
+            if (message.Source != MessageSource.User)
+            {
+                return;
+            }
+
+            var commandContext = new SocketCommandContext(client, message);
+
+            var argumentPosition = 0;
+            if (!message.HasMentionPrefix(client.CurrentUser, ref argumentPosition))
+            {
+                // The message @Bot will come as <@{Id}> but client.CurrentUser.Mention is <@!{Id}>
+                // So to be safe check for both in case it is changed...
+                if (message.Content.Equals(client.CurrentUser.Mention)
+                    || message.Content.Equals(client.CurrentUser.Mention.Replace("!", string.Empty)))
+                {
+                    IResult defaultResponseResult =
+                        await commandService.ExecuteDefaultResponse(commandContext, argumentPosition);
+                    await LogErrorResult(commandContext, defaultResponseResult);
+                }
+
+                argumentPosition = 0;
+                if (!message.HasCharPrefix('!', ref argumentPosition))
+                {
+                    // Not going to respond
+                    return;
+                }
+            }
+
+            IResult result = await commandService.ExecuteAsync(commandContext, argumentPosition);
+
+            await LogErrorResult(commandContext, result);
+        }
+
+        private void LogEnvironment()
+        {
+            logger.LogInformation("Hosting environment: {environment} PID: {PID}", environment.EnvironmentName,
+                Process.GetCurrentProcess().Id);
+        }
+
+        private async Task LogErrorResult(SocketCommandContext commandContext, IResult result)
+        {
+            if (result.Error.HasValue && result.Error.Value != CommandError.UnknownCommand)
+            {
+                await commandContext.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+
+        private void LogStartup()
+        {
+            logger.LogInformation("Bot starting");
+            LogEnvironment();
         }
     }
 }
