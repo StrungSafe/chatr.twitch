318488479708c9f06d1017622bf8ba8fd0c9f4f8
Miguel Molina Jr <molim134@gmail.com>
2020-09-23 14:53:29 -0400

    update ui allow adding sources and destinations

--- Chatr/Chatr.Forms/MainForm.Designer.cs
+++ Chatr/Chatr.Forms/MainForm.Designer.cs
@@ -31,17 +31,34 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.DestinationTextBox = new System.Windows.Forms.TextBox();
+            this.AddDestinationButton = new System.Windows.Forms.Button();
+            this.DestinationsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SourceTextBox = new System.Windows.Forms.TextBox();
+            this.AddSourceButton = new System.Windows.Forms.Button();
+            this.SourcesCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(694, 12);
+            this.button1.Location = new System.Drawing.Point(813, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 195);
             this.button1.TabIndex = 0;
@@ -50,21 +67,151 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(694, 305);
+            this.button2.Location = new System.Drawing.Point(813, 213);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 133);
+            this.button2.Size = new System.Drawing.Size(94, 128);
             this.button2.TabIndex = 1;
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(676, 292);
+            this.groupBox1.Size = new System.Drawing.Size(795, 195);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Twitch";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.DestinationTextBox);
+            this.groupBox4.Controls.Add(this.AddDestinationButton);
+            this.groupBox4.Controls.Add(this.DestinationsCheckedListBox);
+            this.groupBox4.Location = new System.Drawing.Point(478, 22);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(303, 161);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Destinations";
+            // 
+            // DestinationTextBox
+            // 
+            this.DestinationTextBox.Location = new System.Drawing.Point(6, 22);
+            this.DestinationTextBox.Name = "DestinationTextBox";
+            this.DestinationTextBox.Size = new System.Drawing.Size(177, 23);
+            this.DestinationTextBox.TabIndex = 12;
+            // 
+            // AddDestinationButton
+            // 
+            this.AddDestinationButton.Location = new System.Drawing.Point(189, 21);
+            this.AddDestinationButton.Name = "AddDestinationButton";
+            this.AddDestinationButton.Size = new System.Drawing.Size(105, 23);
+            this.AddDestinationButton.TabIndex = 13;
+            this.AddDestinationButton.Text = "Add Destination";
+            this.AddDestinationButton.UseVisualStyleBackColor = true;
+            this.AddDestinationButton.Click += new System.EventHandler(this.AddDestinationButton_Click);
+            // 
+            // DestinationsCheckedListBox
+            // 
+            this.DestinationsCheckedListBox.FormattingEnabled = true;
+            this.DestinationsCheckedListBox.Location = new System.Drawing.Point(6, 55);
+            this.DestinationsCheckedListBox.Name = "DestinationsCheckedListBox";
+            this.DestinationsCheckedListBox.Size = new System.Drawing.Size(177, 94);
+            this.DestinationsCheckedListBox.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.SourceTextBox);
+            this.groupBox3.Controls.Add(this.AddSourceButton);
+            this.groupBox3.Controls.Add(this.SourcesCheckedListBox);
+            this.groupBox3.Location = new System.Drawing.Point(169, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(303, 161);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sources";
+            // 
+            // SourceTextBox
+            // 
+            this.SourceTextBox.Location = new System.Drawing.Point(6, 22);
+            this.SourceTextBox.Name = "SourceTextBox";
+            this.SourceTextBox.Size = new System.Drawing.Size(177, 23);
+            this.SourceTextBox.TabIndex = 12;
+            // 
+            // AddSourceButton
+            // 
+            this.AddSourceButton.Location = new System.Drawing.Point(189, 21);
+            this.AddSourceButton.Name = "AddSourceButton";
+            this.AddSourceButton.Size = new System.Drawing.Size(105, 23);
+            this.AddSourceButton.TabIndex = 13;
+            this.AddSourceButton.Text = "Add Source";
+            this.AddSourceButton.UseVisualStyleBackColor = true;
+            this.AddSourceButton.Click += new System.EventHandler(this.AddSourceButton_Click);
+            // 
+            // SourcesCheckedListBox
+            // 
+            this.SourcesCheckedListBox.FormattingEnabled = true;
+            this.SourcesCheckedListBox.Location = new System.Drawing.Point(6, 55);
+            this.SourcesCheckedListBox.Name = "SourcesCheckedListBox";
+            this.SourcesCheckedListBox.Size = new System.Drawing.Size(177, 94);
+            this.SourcesCheckedListBox.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(63, 74);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(63, 50);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(63, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Channel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Token";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
             // 
             // groupBox2
             // 
@@ -72,35 +219,12 @@
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.textBox2);
             this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 310);
+            this.groupBox2.Location = new System.Drawing.Point(12, 213);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(676, 128);
+            this.groupBox2.Size = new System.Drawing.Size(795, 128);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Discord";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(63, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 23);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(63, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(274, 23);
-            this.textBox2.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 15);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Token";
             // 
             // label2
             // 
@@ -111,17 +235,46 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Channel";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Token";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 23);
+            this.textBox1.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(915, 354);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.Text = "Chatr";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -138,6 +291,20 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckedListBox SourcesCheckedListBox;
+        private System.Windows.Forms.Button AddSourceButton;
+        private System.Windows.Forms.TextBox SourceTextBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox DestinationTextBox;
+        private System.Windows.Forms.Button AddDestinationButton;
+        private System.Windows.Forms.CheckedListBox DestinationsCheckedListBox;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- Chatr/Chatr.Forms/MainForm.cs
+++ Chatr/Chatr.Forms/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿namespace Chatr.Forms
 {
+    using System;
     using System.Windows.Forms;
 
     public partial class MainForm : Form
@@ -9,5 +10,18 @@
             InitializeComponent();
         }
 
+        private void AddDestinationButton_Click(object sender, EventArgs e)
+        {
+            string destinationName = DestinationTextBox.Text;
+
+            DestinationsCheckedListBox.Items.Add(destinationName);
+        }
+
+        private void AddSourceButton_Click(object sender, EventArgs e)
+        {
+            string sourceName = SourceTextBox.Text;
+
+            SourcesCheckedListBox.Items.Add(sourceName);
+        }
     }
 }
