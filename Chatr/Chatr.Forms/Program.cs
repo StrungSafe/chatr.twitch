36a12e901759cf11ca667b3f0fdc22f3d6afe017
Miguel Molina Jr <molim134@gmail.com>
2020-09-25 20:53:07 -0400

    DI baby

--- Chatr/Chatr.Forms/Program.cs
+++ Chatr/Chatr.Forms/Program.cs
@@ -3,6 +3,12 @@ namespace Chatr.Forms
     using System;
     using System.Windows.Forms;
 
+    using Chatr.Core;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
     internal static class Program
     {
         /// <summary>
@@ -14,7 +20,27 @@ namespace Chatr.Forms
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            IHost host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.AddLogging(builder =>
+                {
+                    builder.AddEventLog(settings =>
+                    {
+                        settings.SourceName = context.Configuration["Logging:EventLog:SourceName"];
+                    });
+                });
+
+                services.AddSingleton<Bot>();
+
+                services.AddSingleton<DiscordBot>();
+
+                services.AddSingleton<MainForm>();
+            }).Build();
+
+            var mainForm = host.Services.GetRequiredService<MainForm>();
+
+            Application.Run(mainForm);
         }
     }
 }
